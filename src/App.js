68de4e5e59475c124365370b0186241ad1f05100
KateTref <trefilovaekaterina408@gmail.com>
2022-11-14 23:47:09 +0200

    task 1 add styles and propTypes

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ export default function App() {
         tag = {user.tag}
         location = {user.location}
         avatar = {user.avatar}
-        stats = {user.stats}
+        followers = {user.stats.followers}
+        views = {user.stats.views}
+        likes = {user.stats.likes}
         />
       </div>
         
-       
       </>
     );
   }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,8 @@
 import './profile.css';
+import PropTypes from 'prop-types';
 
 
-const Profile = ({username, tag, location, avatar, stats}) => {
+const Profile = ({username, tag, location, avatar, followers, views, likes}) => {
     return (
 <div className="profile">
   <div className="description">
@@ -18,19 +19,29 @@ const Profile = ({username, tag, location, avatar, stats}) => {
   <ul className="stats">
     <li className="stats__item">
       <span className="label">Followers</span>
-      <span className="quantity">{stats.followers}</span>
+      <span className="quantity">{followers}</span>
     </li>
     <li className="stats__item">
       <span className="label">Views</span>
-      <span className="quantity">{stats.views}</span>
+      <span className="quantity">{views}</span>
     </li>
     <li className="stats__item">
       <span className="label">Likes</span>
-      <span className="quantity">{stats.likes}</span>
+      <span className="quantity">{likes}</span>
     </li>
   </ul>
 </div>
     )
+}
+
+Profile.propTypes = {
+username: PropTypes.string.isRequired,
+tag: PropTypes.string.isRequired,
+location: PropTypes.string.isRequired,
+avatar: PropTypes.string.isRequired,
+followers: PropTypes.number.isRequired,
+views: PropTypes.number.isRequired,
+likes: PropTypes.number.isRequired,
 }
 
 export default Profile;
